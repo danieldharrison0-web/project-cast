5a2a952e938e14d4eb201bd73e1ea0d8b3340cc6
danieldharrison0-web <danieldharrison0@gmail.com>
2026-07-03 23:50:06 +0100

    Add CAST v0.1 interactions

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// CAST interactions
+// CAST v0.1 interactions
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.classList.add("loaded");
+
+  const forms = document.querySelectorAll(".signup");
+
+  forms.forEach((form) => {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+    });
+  });
+});
